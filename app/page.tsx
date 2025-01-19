d97155ee91d00000af259221883a1ffc86bfacdc
RyanSikandar <ryansikandar@gmail.com>
2025-01-19 20:15:19 +0500

    feat: updated routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Practise from "@/components/home/Practise";
 import Page2 from "@/components/home/page2";
+import Login from "./(auth)/login/page";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center space-y-24">
-      <Practise />
+    <main className="flex h-screen flex-col items-center space-y-24">
+      <Login />
     </main>
   );
 }
