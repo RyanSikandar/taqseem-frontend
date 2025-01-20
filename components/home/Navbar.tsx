4685d32bfae312b49e0467c48c9f0e53c73d246e
RyanSikandar <ryansikandar@gmail.com>
2025-01-20 22:27:21 +0500

    feat: css majik

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -5,21 +5,23 @@ import { Search } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { SideMenu } from "./SideMenu";
 import { useNavigation } from "@/context/navigation-context";
+import { Input } from "../ui/input";
 
 export default function Navbar() {
   const { activeTab, setActiveTab } = useNavigation();
 
   return (
     <header className="fixed top-0 left-0 right-0 bg-white border-b z-50 opacity-100 ">
-      <div className="flex items-center justify-between px-4 h-14">
-        <SideMenu />
-        <div className="flex items-center gap-2">
-          <img src="/assets/icons/Taqseem.svg" alt="logo" className="w-25 h-20" />
+      <div className="flex justify-between">
+        <div className="flex items-center justify-between px-4 h-14 w-[55%]">
+          <SideMenu />
+          <div className="flex items-center gap-2">
+            <img src="/assets/icons/Taqseem.svg" alt="logo" className="w-25 h-20" />
+          </div>
         </div>
-
-        <Button variant="ghost" size="icon" className="p-2">
-          <Search className="w-5 h-5" />
-        </Button>
+        <div className="w-[20%]">
+          <Input type="text" placeholder="Search" className="w-full h-10" />
+        </div>
       </div>
 
       {/* Action buttons */}
