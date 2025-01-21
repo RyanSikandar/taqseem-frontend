1f4acc30e3bafb1dbc893afc62d5e59a3145f033
RyanSikandar <ryansikandar@gmail.com>
2025-01-21 21:05:48 +0500

    feat: added implementation of indexed DB or favs

--- context/favourites-context.tsx
+++ context/favourites-context.tsx
@@ -1,28 +1,85 @@
 'use client'
 import { Post } from "@/types";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { openDB, type IDBPDatabase } from 'idb';
 
 interface FavouriteContextType {
     favourites: Post[];
     toggleFavourite: (post: Post) => void;
+    isLoading: boolean;
 }
 
 const FavouritesContext = createContext<FavouriteContextType | undefined>(undefined);
 
+const DB_NAME = 'favourites';
+const DB_VERSION = 1;
+const STORE_NAME = 'posts';
+
+const initDB = async () => {
+    const db = await openDB(DB_NAME, DB_VERSION, {
+        upgrade(database) {
+            database.createObjectStore(STORE_NAME, { keyPath: 'id' });
+        },
+    });
+    return db;
+}
+
 export function FavouritesProvider({ children }: { children: React.ReactNode }) {
     const [favourites, setFavourites] = useState<Post[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [db, setDb] = useState<IDBPDatabase | null>(null);
 
-    const toggleFavourite = (post: Post) => {
-        setFavourites((prev) => {
-            const isFavourite = prev.find((fav) => fav.id === post.id);
-            if (isFavourite) {
-                return prev.filter((fav) => fav.id !== post.id);
+    useEffect(() => {
+        const initialize = async () => {
+            try {
+                const database = await initDB();
+                setDb(db);
+                const data = await database.getAll(STORE_NAME);
+                setFavourites(data);
+                setIsLoading(false);
             }
-            return [...prev, post];
-        })
-    }
+            catch (error) {
+                console.error(error);
+            }
+            finally {
+                setIsLoading(false);
+            }
+        };
+        if (typeof window !== 'undefined') {
+            initialize();
+        }
 
-    return <FavouritesContext.Provider value={{ favourites, toggleFavourite }}>{children}</FavouritesContext.Provider>;
+        return () => {
+            // Cleanup
+            if (db) {
+                db.close();
+            }
+        }
+
+    }, [])
+
+    const toggleFavourite = async (post: Post) => {
+        if (!db) {
+            return;
+        }
+
+        try {
+            const existingPost = await db.get(STORE_NAME, post.id);
+            if (existingPost) {
+                await db.delete(STORE_NAME, post.id);
+                setFavourites(favourites.filter(fav => fav.id !== post.id));
+            } else {
+                await db.add(STORE_NAME, post);
+                setFavourites([...favourites, post]);
+            }
+        }
+        catch (error) {
+            console.error(error);
+
+        }
+    };
+
+    return <FavouritesContext.Provider value={{ favourites, toggleFavourite, isLoading }}>{children}</FavouritesContext.Provider>;
 }
 
 export function useFavourites() {
