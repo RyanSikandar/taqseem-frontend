e7232d288484998b9f782ee5b7dd538f7bc14acb
RyanSikandar <ryansikandar@gmail.com>
2025-01-23 17:10:26 +0500

    feat: added favs page

---
+++ app/favourite/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/home/Navbar";
+import { NavigationProvider } from "@/context/navigation-context";
+
+type LayoutProps = {
+    children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <NavigationProvider>
+            <div className="h-screen bg-white">
+                <Navbar showActionButtons={false} showSearchBar={false} />
+                <main>
+                    {children}
+                </main>
+            </div>
+        </NavigationProvider>
+    );
+}
